e54dcbc889f87ab047f252e7c0b0d2f713e75da5
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-12-16 19:03:40 +0100

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/app/NavBar/NavBar";
-import { SessionProvider } from "@/lib/providers/SessionProvider";
-import { validateRequest } from "@/lib/auth";
-import { Toaster } from "@/components/ui/sonner"
-import { ThemeProvider } from "@/lib/providers/ThemeProvider";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/app/NavBar/NavBar';
+import { SessionProvider } from '@/lib/providers/SessionProvider';
+import { validateRequest } from '@/lib/auth';
+import { Toaster } from '@/components/ui/sonner';
+import { ThemeProvider } from '@/lib/providers/ThemeProvider';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Echospace",
-  description: "User feedback for developers.",
+  title: 'Echospace',
+  description: 'User feedback for developers.',
 };
 
 export default async function RootLayout({
@@ -19,9 +20,16 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const sessionData = await validateRequest()
+  const sessionData = await validateRequest();
   return (
     <html lang="en">
+      {process.env.NODE_ENV === 'production' && (
+        <Script
+          defer
+          src="https://analytics.srizan.dev/ua.js"
+          data-website-id="4564fc34-12f7-4656-9635-9315a48b9339"
+        />
+      )}
       <body className={inter.className}>
         <SessionProvider value={sessionData}>
           <ThemeProvider
@@ -30,9 +38,9 @@ export default async function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-          <Navbar />
-          {children}
-          <Toaster />
+            <Navbar />
+            {children}
+            <Toaster />
           </ThemeProvider>
         </SessionProvider>
       </body>
