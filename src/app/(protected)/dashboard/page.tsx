a880f4f04237ab2e175ba4119a16c0cd5e06bf91
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-12-17 16:25:10 +0100

    feat: no projects found button

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,8 @@
 import ProjectCard from '@/components/app/ProjectCard/ProjectCard';
+import { Button } from '@/components/ui/button';
 import { validateRequest } from '@/lib/auth';
 import prisma from '@/lib/db';
-import { faker } from '@faker-js/faker';
+import Link from 'next/link';
 
 export default async function Page() {
   const { user } = await validateRequest();
@@ -12,6 +13,15 @@ export default async function Page() {
   });
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4">
+      {db.length === 0 && (
+        <div className="col-span-full text-center">
+          <h1 className="text-2xl font-bold">No projects found</h1>
+          <p className="text-muted-foreground">Create a project to get started</p>
+          <Link href="/create">
+            <Button size={'sm'}>Create Project</Button>
+          </Link>
+        </div>
+      )}
       {db.map((d) => (
         <ProjectCard key={d.id} {...d} />
       ))}
