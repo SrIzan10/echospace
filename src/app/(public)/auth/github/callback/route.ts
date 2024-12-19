00ea44fc2ab32763843f3d774228f039209d78ea
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-12-19 22:56:15 +0100

    chore: use octokit to get the user

--- src/app/(public)/auth/github/callback/route.ts
+++ src/app/(public)/auth/github/callback/route.ts
@@ -1,84 +1,82 @@
-import { github, lucia } from "@/lib/auth";
-import { cookies } from "next/headers";
-import { OAuth2RequestError } from "arctic";
-import { generateIdFromEntropySize } from "lucia";
-import prisma from "@/lib/db";
+import { github, lucia } from '@/lib/auth';
+import { cookies } from 'next/headers';
+import { OAuth2RequestError } from 'arctic';
+import { generateIdFromEntropySize } from 'lucia';
+import prisma from '@/lib/db';
+import { Octokit } from '@octokit/core';
 
-// TODO: maybe do the requests with octokit?
 export async function GET(request: Request): Promise<Response> {
-	const url = new URL(request.url);
-	const code = url.searchParams.get("code");
-	const state = url.searchParams.get("state");
-	const storedState = cookies().get("github_oauth_state")?.value ?? null;
-	if (!code || !state || !storedState || state !== storedState) {
-		return new Response(null, {
-			status: 400
-		});
-	}
+  const url = new URL(request.url);
+  const code = url.searchParams.get('code');
+  const state = url.searchParams.get('state');
+  const storedState = cookies().get('github_oauth_state')?.value ?? null;
+  if (!code || !state || !storedState || state !== storedState) {
+    return new Response(null, {
+      status: 400,
+    });
+  }
 
-	try {
-		const tokens = await github.validateAuthorizationCode(code);
-        console.log(tokens);
-		const githubUserResponse = await fetch("https://api.github.com/user", {
-			headers: {
-				Authorization: `Bearer ${tokens.accessToken()}`
-			}
-		});
-		const githubUser: GitHubUser = await githubUserResponse.json();
+  try {
+    const tokens = await github.validateAuthorizationCode(code);
+    const octokit = new Octokit({
+      auth: tokens.accessToken(),
+    });
 
-		const existingUser = await prisma.user.findUnique({
-            where: {
-                githubId: githubUser.id.toString()
-            }
-        });
+    const { data: githubUser } = await octokit.request('GET /user');
 
-		if (existingUser) {
-			const session = await lucia.createSession(existingUser.id, {});
-			const sessionCookie = lucia.createSessionCookie(session.id);
-			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			return new Response(null, {
-				status: 302,
-				headers: {
-					Location: "/"
-				}
-			});
-		}
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        githubId: githubUser.id.toString(),
+      },
+    });
 
-		const userId = generateIdFromEntropySize(10);
+    if (existingUser) {
+      const session = await lucia.createSession(existingUser.id, {});
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+      return new Response(null, {
+        status: 302,
+        headers: {
+          Location: '/',
+        },
+      });
+    }
 
-        await prisma.user.create({
-            data: {
-                id: userId,
-                githubId: githubUser.id.toString(),
-                username: githubUser.login
-            }
-        })
+    const userId = generateIdFromEntropySize(10);
 
-		const session = await lucia.createSession(userId, {});
-		const sessionCookie = lucia.createSessionCookie(session.id);
-		cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-		return new Response(null, {
-			status: 302,
-			headers: {
-				Location: "/"
-			}
-		});
-	} catch (e) {
-		// the specific error message depends on the provider
-        console.error(e);
-		if (e instanceof OAuth2RequestError) {
-			// invalid code
-			return new Response(null, {
-				status: 400
-			});
-		}
-		return new Response(null, {
-			status: 500
-		});
-	}
+    await prisma.user.create({
+      data: {
+        id: userId,
+        githubId: githubUser.id.toString(),
+        username: githubUser.login,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: '/',
+      },
+    });
+  } catch (e) {
+    // the specific error message depends on the provider
+    console.error(e);
+    if (e instanceof OAuth2RequestError) {
+      // invalid code
+      return new Response(null, {
+        status: 400,
+      });
+    }
+    return new Response(null, {
+      status: 500,
+    });
+  }
 }
 
 interface GitHubUser {
-	id: number;
-	login: string;
+  id: number;
+  login: string;
 }
