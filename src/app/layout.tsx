77a493944fbd189d3b628aa846693fe38232a0fc
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-03-09 00:15:37 +0100

    chore: oops

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,9 @@ const jbmono = JetBrains_Mono({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Echospace',
   description: 'User feedback for developers.',
-  // @ts-ignore
-  'darkreader-lock': 'asdf'
+  other: {
+    'darkreader-lock': 'asdf',
+  }
 };
 
 export default async function RootLayout({
