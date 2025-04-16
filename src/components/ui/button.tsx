7c21b1b9b615f59da30e945dc13ef1d72eb432c4
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2025-04-16 14:27:30 +0200

    revert: "chore: ref codemod from react"
    This reverts commit efa082f62332fa24519a0b99f23cdb52da32cd3f.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -42,32 +42,22 @@ export interface ButtonProps
   loading?: boolean;
 }
 
-const Button = (
-  {
-    ref,
-    className,
-    variant,
-    size,
-    asChild = false,
-    loading,
-    ...props
-  }: ButtonProps & {
-    ref: React.RefObject<HTMLButtonElement>;
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, loading, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        disabled={loading}
+        ref={ref}
+        {...props}
+      >
+        {loading && <LoaderCircle className={cn('h-4 w-4 animate-spin', props.children && 'mr-2')} />}
+        {props.children}
+      </Comp>
+    )
   }
-) => {
-  const Comp = asChild ? Slot : "button"
-  return (
-    <Comp
-      className={cn(buttonVariants({ variant, size, className }))}
-      disabled={loading}
-      ref={ref}
-      {...props}
-    >
-      {loading && <LoaderCircle className={cn('h-4 w-4 animate-spin', props.children && 'mr-2')} />}
-      {props.children}
-    </Comp>
-  )
-}
+)
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
